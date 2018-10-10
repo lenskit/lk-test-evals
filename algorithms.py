56c94d79870b22690f59925b82a2d7ce9701ecaa
Michael Ekstrand <md@ekstrandom.net>
2018-10-10 16:32:23 -0600

    small config changes

--- algorithms.py
+++ algorithms.py
@@ -8,4 +8,4 @@ item_item = item_knn.ItemItem(20, min_sim=1.0e-6)
 
 funksvd = funksvd.FunkSVD(15, iterations=125, lrate=0.001)
 
-als = als.BiasedMF(50, iterations=20)
+als = als.BiasedMF(50, iterations=30, reg=0.1)
