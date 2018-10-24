8e0eed1aaaad77a56251cdfca79ee16b29bff6d4
Michael Ekstrand <michaelekstrand@boisestate.edu>
2018-10-24 11:44:31 -0600

    limit neighborhood sizes

--- algorithms.py
+++ algorithms.py
@@ -4,7 +4,7 @@ bias = basic.Bias(damping=5)
 
 user_user = user_knn.UserUser(30, min_sim=1.0e-6)
 
-item_item = item_knn.ItemItem(20, min_sim=1.0e-6)
+item_item = item_knn.ItemItem(20, min_sim=1.0e-6, save_nbrs=5000)
 
 funksvd = funksvd.FunkSVD(15, iterations=125, lrate=0.001)
 
